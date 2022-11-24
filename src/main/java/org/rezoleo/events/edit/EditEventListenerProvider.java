2fea30a1e418f17451d8fa363d7b55c2962793c3
Léo Salé <leo.sale72@gmail.com>
2022-11-24 19:15:03 +0100

    retrieve user update on event

--- src/main/java/org/rezoleo/events/edit/EditEventListenerProvider.java
+++ src/main/java/org/rezoleo/events/edit/EditEventListenerProvider.java
@@ -3,27 +3,32 @@ package org.rezoleo.events.edit;
 import org.jboss.logging.Logger;
 import org.keycloak.events.Event;
 import org.keycloak.events.EventListenerProvider;
+import org.keycloak.events.EventType;
 import org.keycloak.events.admin.AdminEvent;
 import org.keycloak.events.admin.OperationType;
 import org.keycloak.events.admin.ResourceType;
 import twitter4j.JSONObject;
+
+import java.util.Map;
 
 public class EditEventListenerProvider implements EventListenerProvider {
     private static final Logger log = Logger.getLogger(EditEventListenerProvider.class);
 
     @Override
     public void onEvent(Event event) {
-        // TODO: handle only update event
         // TODO: filter specific client if update originated from him?
-        System.out.println("EVENT OCCURED: " + event.getType().toString());
-        System.out.println("DETAILS: " + event.getDetails().toString());
+        if (isUserUpdate(event)) {
+            log.info("User update on " + getUserId(event));
+            System.out.println("DETAILS: " + event.getDetails().toString());
+            userToJson(event);
+        }
     }
 
     @Override
     public void onEvent(AdminEvent adminEvent, boolean b) {
         if (isUserUpdate(adminEvent)) {
             log.info("User update on " + getUserId(adminEvent));
-            userToJson(adminEvent.getRepresentation());
+            userToJson(adminEvent);
         }
     }
 
@@ -32,13 +37,38 @@ public class EditEventListenerProvider implements EventListenerProvider {
 
     }
 
+    private boolean isUserUpdate(Event event) {
+        boolean isUpdate = event.getType() == EventType.UPDATE_PROFILE;
+        return isUpdate;
+    }
+
     private boolean isUserUpdate(AdminEvent adminEvent) {
         boolean isUpdate = adminEvent.getOperationType() == OperationType.UPDATE;
         boolean isUserResource = adminEvent.getResourceType() == ResourceType.USER;
         return isUpdate && isUserResource;
     }
 
-    private String userToJson(String user) {
+    private String userToJson(Event event) {
+        Map<String, String> changes = event.getDetails();
+        JSONObject userJson = new JSONObject();
+        addUpdatedValueToJson(changes, userJson, "updated_first_name", "firstname");
+        addUpdatedValueToJson(changes, userJson, "updated_last_name", "lastname");
+        addUpdatedValueToJson(changes, userJson, "updated_email", "email");
+//        if (json.has("attributes") && json.getJSONObject("attributes").has("room")) {
+//            userJson.put("room", json.getJSONObject("attributes").getJSONArray("room").getString(0));
+//        }
+        log.debug(userJson.toString());
+        return userJson.toString();
+    }
+
+    private void addUpdatedValueToJson(Map<String, String> changes, JSONObject userJson, String updatedValue, String jsonKey) {
+        if (changes.containsKey(updatedValue)) {
+            userJson.put(jsonKey, changes.get(updatedValue));
+        }
+    }
+
+    private String userToJson(AdminEvent adminEvent) {
+        String user = adminEvent.getRepresentation();
         JSONObject userJson = new JSONObject();
         JSONObject json = new JSONObject(user);
         userJson.put("firstname", json.getString("firstName"));
@@ -49,6 +79,10 @@ public class EditEventListenerProvider implements EventListenerProvider {
         }
         log.debug(userJson.toString());
         return userJson.toString();
+    }
+
+    private String getUserId(Event event) {
+        return event.getUserId();
     }
 
     private String getUserId(AdminEvent adminEvent) {
