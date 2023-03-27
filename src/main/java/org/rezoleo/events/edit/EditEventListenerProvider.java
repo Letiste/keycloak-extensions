1c74e6ec505d82b1f4c85ef22da00eb957d83bb0
Léo Salé <leo.sale72@gmail.com>
2023-03-27 13:01:43 +0200

    add validator room

--- src/main/java/org/rezoleo/events/edit/EditEventListenerProvider.java
+++ src/main/java/org/rezoleo/events/edit/EditEventListenerProvider.java
@@ -1,144 +1,144 @@
-package org.rezoleo.events.edit;
+ package org.rezoleo.events.edit;
 
-import org.jboss.logging.Logger;
-import org.keycloak.events.Event;
-import org.keycloak.events.EventListenerProvider;
-import org.keycloak.events.EventType;
-import org.keycloak.events.admin.AdminEvent;
-import org.keycloak.events.admin.OperationType;
-import org.keycloak.events.admin.ResourceType;
-import twitter4j.JSONObject;
+ import org.jboss.logging.Logger;
+ import org.json.JSONObject;
+ import org.keycloak.events.Event;
+ import org.keycloak.events.EventListenerProvider;
+ import org.keycloak.events.EventType;
+ import org.keycloak.events.admin.AdminEvent;
+ import org.keycloak.events.admin.OperationType;
+ import org.keycloak.events.admin.ResourceType;
 
-import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.time.Duration;
-import java.util.Map;
+ import java.io.IOException;
+ import java.net.URI;
+ import java.net.http.HttpClient;
+ import java.net.http.HttpRequest;
+ import java.net.http.HttpResponse;
+ import java.time.Duration;
+ import java.util.Map;
 
-public class EditEventListenerProvider implements EventListenerProvider {
-    private static final Logger log = Logger.getLogger(EditEventListenerProvider.class);
-    private static final int REQUEST_TIMEOUT = 5;
-    private final HttpClient client;
-    private final String editUserEventNotificationUrl;
+ public class EditEventListenerProvider implements EventListenerProvider {
+     private static final Logger log = Logger.getLogger(EditEventListenerProvider.class);
+     private static final int REQUEST_TIMEOUT = 5;
+     private final HttpClient client;
+     private final String editUserEventNotificationUrl;
 
-    public EditEventListenerProvider(HttpClient client, String editUserEventNotificationUrl) {
-        this.client = client;
-        this.editUserEventNotificationUrl = editUserEventNotificationUrl;
-    }
+     public EditEventListenerProvider(HttpClient client, String editUserEventNotificationUrl) {
+         this.client = client;
+         this.editUserEventNotificationUrl = editUserEventNotificationUrl;
+     }
 
-    @Override
-    public void onEvent(Event event) {
-        // TODO: filter specific client if update originated from him?
-        // TODO: test with room
-        if (isUserUpdate(event)) {
-            log.info("User update on " + getUserId(event));
-            System.out.println("DETAILS: " + event.getDetails().toString());
-            String userJson = userToJson(event);
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create(this.editUserEventNotificationUrl))
-                    .method("PATCH", HttpRequest.BodyPublishers.ofString(userJson))
-                    .header("Content-Type", "application/json")
-                    .timeout(Duration.ofSeconds(REQUEST_TIMEOUT))
-                    .build();
-            try {
-                log.info("Send user update notifcation");
-                HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-                log.info("User update notification sent: " + response);
-            } catch (IOException e) {
-                log.error(e.toString());
-            } catch (InterruptedException e) {
-                log.error(e.toString());
-            }
-        }
-    }
+     @Override
+     public void onEvent(Event event) {
+         // TODO: filter specific client if update originated from him?
+         // TODO: test with room
+//         if (isUserUpdate(event)) {
+//             log.info("User update on " + getUserId(event));
+//             System.out.println("DETAILS: " + event.getDetails().toString());
+//             String userJson = userToJson(event);
+//             HttpRequest request = HttpRequest.newBuilder()
+//                     .uri(URI.create(this.editUserEventNotificationUrl))
+//                     .method("PATCH", HttpRequest.BodyPublishers.ofString(userJson))
+//                     .header("Content-Type", "application/json")
+//                     .timeout(Duration.ofSeconds(REQUEST_TIMEOUT))
+//                     .build();
+//             try {
+//                 log.info("Send user update notifcation");
+//                 HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+//                 log.info("User update notification sent: " + response);
+//             } catch (IOException e) {
+//                 log.error(e.toString());
+//             } catch (InterruptedException e) {
+//                 log.error(e.toString());
+//             }
+//         }
+     }
 
-    @Override
-    public void onEvent(AdminEvent adminEvent, boolean b) {
-        if (isUserUpdate(adminEvent)) {
-            log.info("User update on " + getUserId(adminEvent));
-            String userJson = userToJson(adminEvent);
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create(this.editUserEventNotificationUrl))
-                    .method("PATCH", HttpRequest.BodyPublishers.ofString(userJson))
-                    .header("Content-Type", "application/json")
-                    .timeout(Duration.ofSeconds(REQUEST_TIMEOUT))
-                    .build();
-            try {
-                log.info("Send user update notifcation");
-                HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-                log.info("User update notification sent: " + response);
-            } catch (IOException e) {
-                log.error(e.toString());
-            } catch (InterruptedException e) {
-                log.error(e.toString());
-            }
-        }
-    }
+     @Override
+     public void onEvent(AdminEvent adminEvent, boolean b) {
+//         if (isUserUpdate(adminEvent)) {
+//             log.info("User update on " + getUserId(adminEvent));
+//             String userJson = userToJson(adminEvent);
+//             HttpRequest request = HttpRequest.newBuilder()
+//                     .uri(URI.create(this.editUserEventNotificationUrl))
+//                     .method("PATCH", HttpRequest.BodyPublishers.ofString(userJson))
+//                     .header("Content-Type", "application/json")
+//                     .timeout(Duration.ofSeconds(REQUEST_TIMEOUT))
+//                     .build();
+//             try {
+//                 log.info("Send user update notifcation");
+//                 HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+//                 log.info("User update notification sent: " + response);
+//             } catch (IOException e) {
+//                 log.error(e.toString());
+//             } catch (InterruptedException e) {
+//                 log.error(e.toString());
+//             }
+//         }
+     }
 
-    @Override
-    public void close() {
+     @Override
+     public void close() {
 
-    }
+     }
 
-    private boolean isUserUpdate(Event event) {
-        return event.getType() == EventType.UPDATE_PROFILE;
-    }
+     private boolean isUserUpdate(Event event) {
+         return event.getType() == EventType.UPDATE_PROFILE;
+     }
 
-    private boolean isUserUpdate(AdminEvent adminEvent) {
-        boolean isUpdate = adminEvent.getOperationType() == OperationType.UPDATE;
-        boolean isUserResource = adminEvent.getResourceType() == ResourceType.USER;
-        return isUpdate && isUserResource;
-    }
+     private boolean isUserUpdate(AdminEvent adminEvent) {
+         boolean isUpdate = adminEvent.getOperationType() == OperationType.UPDATE;
+         boolean isUserResource = adminEvent.getResourceType() == ResourceType.USER;
+         return isUpdate && isUserResource;
+     }
 
-    private String userToJson(Event event) {
-        Map<String, String> changes = event.getDetails();
-        JSONObject userJson = new JSONObject();
-        userJson.put("sso_id", getUserId(event));
-        addUpdatedValueToJson(changes, userJson, "updated_first_name", "firstname");
-        addUpdatedValueToJson(changes, userJson, "updated_last_name", "lastname");
-        addUpdatedValueToJson(changes, userJson, "updated_email", "email");
-//        if (json.has("attributes") && json.getJSONObject("attributes").has("room")) {
-//            userJson.put("room", json.getJSONObject("attributes").getJSONArray("room").getString(0));
-//        }
-        log.debug(userJson.toString());
-        return userJson.toString();
-    }
+     private String userToJson(Event event) {
+         Map<String, String> changes = event.getDetails();
+         JSONObject userJson = new JSONObject();
+         userJson.put("sso_id", getUserId(event));
+         addUpdatedValueToJson(changes, userJson, "updated_first_name", "firstname");
+         addUpdatedValueToJson(changes, userJson, "updated_last_name", "lastname");
+         addUpdatedValueToJson(changes, userJson, "updated_email", "email");
+ //        if (json.has("attributes") && json.getJSONObject("attributes").has("room")) {
+ //            userJson.put("room", json.getJSONObject("attributes").getJSONArray("room").getString(0));
+ //        }
+         log.debug(userJson.toString());
+         return userJson.toString();
+     }
 
-    private void addUpdatedValueToJson(Map<String, String> changes, JSONObject userJson, String updatedValue, String jsonKey) {
-        if (changes.containsKey(updatedValue)) {
-            userJson.put(jsonKey, changes.get(updatedValue));
-        }
-    }
+     private void addUpdatedValueToJson(Map<String, String> changes, JSONObject userJson, String updatedValue, String jsonKey) {
+         if (changes.containsKey(updatedValue)) {
+             userJson.put(jsonKey, changes.get(updatedValue));
+         }
+     }
 
-    private String userToJson(AdminEvent adminEvent) {
-        String user = adminEvent.getRepresentation();
-        JSONObject userJson = new JSONObject();
-        JSONObject json = new JSONObject(user);
-        userJson.put("sso_id", getUserId(adminEvent));
-        addValueToJson(userJson, json, "firstName", "firstname");
-        addValueToJson(userJson, json, "lastName", "lastname");
-        addValueToJson(userJson, json, "email", "email");
-        if (json.has("attributes") && json.getJSONObject("attributes").has("room")) {
-            userJson.put("room", json.getJSONObject("attributes").getJSONArray("room").getString(0));
-        }
-        log.debug(userJson.toString());
-        return userJson.toString();
-    }
+     private String userToJson(AdminEvent adminEvent) {
+         String user = adminEvent.getRepresentation();
+         JSONObject userJson = new JSONObject();
+         JSONObject json = new JSONObject(user);
+         userJson.put("sso_id", getUserId(adminEvent));
+         addValueToJson(userJson, json, "firstName", "firstname");
+         addValueToJson(userJson, json, "lastName", "lastname");
+         addValueToJson(userJson, json, "email", "email");
+         if (json.has("attributes") && json.getJSONObject("attributes").has("room")) {
+             userJson.put("room", json.getJSONObject("attributes").getJSONArray("room").getString(0));
+         }
+         log.debug(userJson.toString());
+         return userJson.toString();
+     }
 
-    private void addValueToJson(JSONObject userJson, JSONObject json, String updatedValue, String jsonKey) {
-        if (json.has(updatedValue)) {
-            userJson.put(jsonKey, json.getString(updatedValue));
-        }
-    }
+     private void addValueToJson(JSONObject userJson, JSONObject json, String updatedValue, String jsonKey) {
+         if (json.has(updatedValue)) {
+             userJson.put(jsonKey, json.getString(updatedValue));
+         }
+     }
 
-    private String getUserId(Event event) {
-        return event.getUserId();
-    }
+     private String getUserId(Event event) {
+         return event.getUserId();
+     }
 
-    private String getUserId(AdminEvent adminEvent) {
-        String[] resourcePath = adminEvent.getResourcePath().split("/");
-        return resourcePath[resourcePath.length - 1];
-    }
-}
+     private String getUserId(AdminEvent adminEvent) {
+         String[] resourcePath = adminEvent.getResourcePath().split("/");
+         return resourcePath[resourcePath.length - 1];
+     }
+ }
---
+++ src/main/java/org/rezoleo/userprofile/validator/RoomValidator.java
@@ -0,0 +1,80 @@
+package org.rezoleo.userprofile.validator;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+import org.jboss.logging.Logger;
+import org.keycloak.models.UserModel;
+import org.keycloak.provider.ConfiguredProvider;
+import org.keycloak.provider.ProviderConfigProperty;
+import org.keycloak.userprofile.UserProfileAttributeValidationContext;
+import org.keycloak.validate.AbstractStringValidator;
+import org.keycloak.validate.ValidationContext;
+import org.keycloak.validate.ValidationError;
+import org.keycloak.validate.ValidatorConfig;
+
+public class RoomValidator extends AbstractStringValidator implements ConfiguredProvider {
+
+    public static final String ID = "room";
+    private static final Logger log = Logger.getLogger(RoomValidator.class);
+
+    protected static final Pattern PATTERN = Pattern.compile("([A-Fa-f][0-3][0-9]{2}[a-b]?|[Dd][Ff][1-4])");
+
+    public static final RoomValidator INSTANCE = new RoomValidator();
+
+    public static final String MESSAGE_NO_MATCH = "room-invalid";
+
+    public static final String CFG_ERROR_MESSAGE = "error-message";
+
+    private static final List<ProviderConfigProperty> configProperties = new ArrayList<>();
+
+    static {
+        ProviderConfigProperty property;
+        property = new ProviderConfigProperty();
+        property.setName(CFG_ERROR_MESSAGE);
+        property.setLabel("Error message key");
+        property.setHelpText("Key of the error message in i18n bundle. Dafault message key is " + MESSAGE_NO_MATCH);
+        property.setType(ProviderConfigProperty.STRING_TYPE);
+        configProperties.add(property);
+    }
+
+    @Override
+    public String getId() {
+        return ID;
+    }
+
+    @Override
+    protected void doValidate(String value, String inputHint, ValidationContext context, ValidatorConfig config) {
+        log.info("VALIDATING ROOM");
+        if (!PATTERN.matcher(value).matches()) {
+            context.addError(new ValidationError(ID, inputHint, "The room does not have the correct format"));
+        }
+
+        UserModel currentUser = ((UserProfileAttributeValidationContext) context).getAttributeContext().getUser();
+        List<UserModel> usersInCurrentRoom = context.getSession().users().searchForUserByUserAttributeStream(context.getSession().getContext().getRealm(), "room", value).collect(Collectors.toList());
+
+        boolean isRoomTaken;
+        if (currentUser == null) {
+            isRoomTaken = !usersInCurrentRoom.isEmpty();
+        } else {
+            isRoomTaken = !usersInCurrentRoom.isEmpty() && !currentUser.getId().equals(usersInCurrentRoom.get(0).getId());
+        }
+        if (isRoomTaken) {
+            context.addError(new ValidationError(ID, inputHint, "this room already belongs to another user"));
+        }
+    }
+
+
+    @Override
+    public String getHelpText() {
+        return "Ensure room is unique.";
+    }
+
+    @Override
+    public List<ProviderConfigProperty> getConfigProperties() {
+        return configProperties;
+    }
+
+}
